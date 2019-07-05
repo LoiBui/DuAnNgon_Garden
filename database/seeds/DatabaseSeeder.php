783524c1ecf0a88caa2c1c510eea0f05e0746a92
Bùi Công Lợi <hts.congloi@gmail.com>
2019-07-05 09:07:07 +0700

    fix route

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,13 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(TaiKhoans::class);
-        // $this->call(Bans::class);
-        // $this->call(DatBans::class);
-        // $this->call(ThucDons::class);
-        // $this->call(PhieuOrders::class);
+        $this->call(TaiKhoans::class);
+        $this->call(Bans::class);
+        $this->call(DatBans::class);
+        $this->call(ThucDons::class);
+        $this->call(PhieuOrders::class);
          $this->call(ChiTietPhieus::class);
-        // $this->call(HoaDons::class);     
-        // $this->call(XuLySuCos::class);
+        $this->call(HoaDons::class);     
+        $this->call(XuLySuCos::class);
     }
 }
